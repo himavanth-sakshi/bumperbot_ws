25ee7b6de1f6fdb8a3fcec7c0ae0f0d21ab3e0d3
hima_ubuntu <himavanth.akhil@gmail.com>
2025-05-18 16:44:02 +0000

    add description

--- build/bumperbot_py_examples/prefix_override/sitecustomize.py
+++ build/bumperbot_py_examples/prefix_override/sitecustomize.py
@@ -1,3 +1,4 @@
 import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/hima/bumperbot_ws/install/bumperbot_py_examples'
+if sys.prefix == '/usr':
+    sys.real_prefix = sys.prefix
+    sys.prefix = sys.exec_prefix = '/home/vboxuser/bumperbot_ws/install/bumperbot_py_examples'
